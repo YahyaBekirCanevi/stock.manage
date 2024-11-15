b026f728c075c9ace61e729dd0f85cbc8329640f
YahyaBekirCanevi <93769780+YahyaBekirCanevi@users.noreply.github.com>
2024-11-15 17:56:24 +0300

    product dto id changed to nullable
    due to creating a product does not require id field in product dto

--- src/main/kotlin/com/canevi/stock/manage/web/dto/ProductDTO.kt
+++ src/main/kotlin/com/canevi/stock/manage/web/dto/ProductDTO.kt
@@ -3,7 +3,7 @@ package com.canevi.stock.manage.web.dto
 import com.canevi.stock.manage.document.Product
 
 data class ProductDTO(
-    val id: String,
+    val id: String? = null,
     val name: String,
     val description: String,
     val price: Double,
